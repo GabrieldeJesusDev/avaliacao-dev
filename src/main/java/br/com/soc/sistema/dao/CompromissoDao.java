4f0410c77a326b728f539c689a12de17e63eb6ab
GabrieldeJesusDev <gaabrieljesus@hotmail.com>
2026-09-11 01:23:48 -0300

    inicia persistencia de compromisso

---
+++ src/main/java/br/com/soc/sistema/dao/CompromissoDao.java
@@ -0,0 +1,62 @@
+package br.com.soc.sistema.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import br.com.soc.sistema.vo.CompromissoVo;
+
+public class CompromissoDao extends Dao {
+
+	public List<CompromissoVo> findAllCompromissos(){
+		StringBuilder query = new StringBuilder("SELECT rowid id, cd_funcionario cdFuncionario, cd_agenda cdAgenda, data, hora FROM compromissos");
+		
+		try(Connection con = getConexao();
+			PreparedStatement ps = con.prepareStatement(query.toString());
+			ResultSet rs = ps.executeQuery()){
+			
+			CompromissoVo vo = null;
+			
+			List<CompromissoVo> compromissos = new ArrayList<>();
+			while(rs.next()) {
+				vo = new CompromissoVo();
+				vo.setRowid(rs.getString("id"));
+				vo.setCdFuncionario(rs.getInt("cdFuncionario"));
+				vo.setCdAgenda(rs.getInt("cdAgenda"));
+				vo.setData(rs.getDate("data").toLocalDate());
+				vo.setHora(rs.getTime("hora").toLocalTime());
+				
+				compromissos.add(vo);
+			}
+			return compromissos;
+			
+		}catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return Collections.emptyList();
+	}
+	
+	public void insertCompromisso(CompromissoVo compromissoVo) {
+		StringBuilder query = new StringBuilder("INSERT INTO compromisso (cd_funcionario, cd_agenda, data, hora) VALUES (?,?,?,?)");
+		
+		try(Connection con = getConexao();
+				PreparedStatement ps = con.prepareStatement(query.toString())){
+			
+			int i=1;
+			
+			ps.setInt(i++, compromissoVo.getCdFuncionario());
+			ps.setInt(i++, compromissoVo.getCdAgenda());
+			ps.setDate(i++, java.sql.Date.valueOf(compromissoVo.getData()));
+			ps.setTime(i++, java.sql.Time.valueOf(compromissoVo.getHora()));
+			ps.executeUpdate();
+			
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
--- src/main/java/br/com/soc/sistema/vo/AgendaVo.java
+++ src/main/java/br/com/soc/sistema/vo/AgendaVo.java
@@ -1,19 +1,20 @@
 package br.com.soc.sistema.vo;
 
 public class AgendaVo {
-	
+
 	private String rowid;
 	private String nome;
 	private Integer periodo;
-	
-	public AgendaVo() {}
+
+	public AgendaVo() {
+	}
 
 	public AgendaVo(String rowid, String nome, Integer periodo) {
 		this.rowid = rowid;
 		this.nome = nome;
 		this.periodo = periodo;
 	}
-	
+
 	public String getNome() {
 		return nome;
 	}
@@ -29,7 +30,7 @@ public class AgendaVo {
 	public void setPeriodo(Integer periodo) {
 		this.periodo = periodo;
 	}
-	
+
 	public void setRowid(String rowid) {
 		this.rowid = rowid;
 	}
@@ -38,5 +39,9 @@ public class AgendaVo {
 		return rowid;
 	}
 
-	
+	@Override
+	public String toString() {
+		return "AgendaVo [rowid=" + rowid + ", nome=" + nome + ", periodo=" + periodo + "]";
+	}
+
 }
